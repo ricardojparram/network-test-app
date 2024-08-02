4c5ed1fc5ce1e58880a02a4171db641decec846f
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-02 12:19:10 -0400

    feat: subtitle added

--- src/app.tsx
+++ src/app.tsx
@@ -5,8 +5,12 @@ export function App() {
   return (
     <>
       <div class="absolute top-0 z-[-2] h-screen w-screen bg-white bg-[radial-gradient(100%_50%_at_50%_10%,rgba(0,163,255,0.13)_0,rgba(0,163,255,0)_50%,rgba(0,163,255,0)_100%)]"></div>
-      <header class="pt-6 flex justify-center">
-        <GradientTypography size="6xl">NETEST</GradientTypography>
+      <header class="pt-16 flex items-center justify-center flex-col">
+        <GradientTypography size="7xl">NETEST</GradientTypography>
+        <h2 class="font-semibold text-xl text-slate-700 pt-2 text-center">
+          Test the <strong>connection's speed</strong> of your{" "}
+          <strong>network</strong>.
+        </h2>
       </header>
       <Test />
     </>
