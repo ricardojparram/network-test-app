955fa910fc664dc303310d164f033f3b0477b481
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-02 22:31:39 -0400

    feat: new animation for Test button

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -5,7 +5,7 @@ export function Test() {
   const [status, setStatus] = useState(false);
   return (
     <>
-      <main class="flex justify-center m-auto">
+      <main class="flex justify-center my-auto mt-[16vh]">
         <div>
           <h4
             class={`${
@@ -16,12 +16,14 @@ export function Test() {
           </h4>
         </div>
         <div>
-          <button class="w-[200px] h-[200px] relative inline-flex items-center justify-center p-1 mb-2 me-2 overflow-hidden text-sm font-medium rounded-full group bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 shadow-lg shadow-blue-300/50">
-            <span class="pulse-fade-in relative p-2 bg-white rounded-full w-full h-full flex items-center justify-center flex-wrap">
-              <GradientTypography
-                size="5xl"
-                hover="group-hover:bg-gradient-to-bl"
-              >
+          <button
+            class="w-[200px] h-[200px] relative inline-flex items-center justify-center p-1 mb-2 me-2
+                   overflow-hidden text-sm font-medium rounded-full group transition-all duration-300 bg-size-200
+                   bg-pos-0 hover:bg-pos-80 bg-gradient-to-br from-green-400 to-blue-600 focus:ring-4 focus:outline-none focus:ring-green-200
+                   shadow-lg hover:shadow-xl hover:shadow-blue-300/50 hover:scale-105 shadow-blue-400/50"
+          >
+            <span class="relative p-2 bg-light dark:bg-dark rounded-full w-full h-full flex items-center justify-center flex-wrap">
+              <GradientTypography size="5xl" hover="group-hover:bg-pos-80">
                 GO
               </GradientTypography>
             </span>
