bd3cd00c609c4095515a46e9efb96963462463ca
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-08-02 23:01:24 -0400

    feat: gradient typography with responsive

--- src/components/GradientTypography.tsx
+++ src/components/GradientTypography.tsx
@@ -7,14 +7,14 @@ type Props = {
 };
 const sizes = {
   xl: "text-xl",
-  "2xl": "text-2xl",
-  "3xl": "text-3xl",
-  "4xl": "text-4xl",
-  "5xl": "text-5xl",
-  "6xl": "text-6xl",
-  "7xl": "text-7xl",
-  "8xl": "text-8xl",
-  "9xl": "text-9xl",
+  "2xl": "text-xl md:text-2xl",
+  "3xl": "text-2xl md:text-3xl",
+  "4xl": "text-3xl md:text-4xl",
+  "5xl": "text-4xl md:text-5xl",
+  "6xl": "text-5xl md:text-6xl",
+  "7xl": "text-6xl md:text-7xl",
+  "8xl": "text-7xl md:text-8xl",
+  "9xl": "text-8xl md:text-9xl",
 };
 export function GradientTypography({ children, size = "6xl", hover }: Props) {
   return (
