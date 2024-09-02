e9bc5b55430d35a6b08b7de6a158a47cc7df41b3
Ricardo Parra <ricardo.prxr16@gmail.com>
2024-09-02 13:30:07 -0400

    feat: types for ip api

---
+++ astro-migration/src/types/userInfo.tsx
@@ -0,0 +1,11 @@
+export interface userInfo {
+  ip: string;
+  country: string;
+  flag: string;
+  region: string;
+  city: string;
+  countryRegion: string;
+  latitude: string;
+  longitude: string;
+  asOrganization: string;
+}
